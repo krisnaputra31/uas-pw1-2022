d87c1e09d894f3ca5cddc13fa21c9f582c5e7e16
Krisna31 <105723874+krisnaputra31@users.noreply.github.com>
2022-12-12 09:26:08 +0700

    close onclick navbar collapse

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
-console.log("hello")
+const navLinks = document.querySelectorAll(".nav-item");
+const collapsable = document.getElementById("navbarNav");
+navLinks.forEach((navLink) => navLink.addEventListener("click", (_) => collapsable.classList.toggle("collapsing")));
