5e24e9ed9a56f70edeecb15e94a764ea5574ef60
krisnaputra31 <105723874+krisnaputra31@users.noreply.github.com>
2022-12-12 11:19:26 +0700

    12-12-2022 fix responsive, change mdn to locally, just add data-aos lib

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-const navLinks = document.querySelectorAll(".nav-item");
-const collapsable = document.getElementById("navbarNav");
-navLinks.forEach((navLink) => navLink.addEventListener("click", (_) => collapsable.classList.toggle("collapsing")));
+AOS.init();
+if (window.innerWidth < 992) {
+  const navLinks = document.querySelectorAll(".nav-item");
+  const collapsable = document.getElementById("navbarNav");
+  navLinks.forEach((navLink) => navLink.addEventListener("click", (_) => collapsable.classList.remove("show")));
+}
